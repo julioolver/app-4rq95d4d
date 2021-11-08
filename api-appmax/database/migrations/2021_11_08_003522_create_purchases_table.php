f97dc667eb23f102c221432f23edc9ee66d360f8
Julio Cesar <juliocesar.olver@gmail.com>
2021-11-07 21:38:39 -0300

    criado demais Models necessários para a API.

--- api-appmax/database/migrations/2021_11_08_003522_create_purchases_table.php
+++ api-appmax/database/migrations/2021_11_08_003522_create_purchases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreatePurchasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('purchases', function (Blueprint $table) {
             $table->id();
-            $table->string('sku')->unique();
-            $table->string('name');
-            $table->integer('quantity')->default(0);
-
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('purchases');
     }
 }
